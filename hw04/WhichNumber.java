d4d93d121b1609b9b522ef58e16eb57e14ff72fd
Ji Qi <jiq217@lehigh.edu>
2015-02-16 05:08:37 +0000

    updated WhichNumber

--- hw04/WhichNumber.java
+++ hw04/WhichNumber.java
@@ -2,87 +2,92 @@
 //homework4-WhichNumber
 //02-15-15
 
-import java.util.Scanner; //import
-
+import java.util.Scanner; //import Scanner
 public class WhichNumber {
 	// main method required for every Java program
  public static void main(String [] args){
-	 Scanner sc = new Scanner(System.in);
+	 Scanner sc = new Scanner(System.in);//declare and constrect
 	 String yes="y",
 			YES="Y",
 			no="n",
-			NO="N";//declare input
+			NO="N";//declare inputs
 	 
 	 System.out.println("Think of a number between 1 and 10 inclusive.");
 	
 	 System.out.println("Is the number even?");
-	 String a = sc.nextLine();//type the correct character to answer the question
-	 if (a.equals(yes)| a.equals(YES)) //determine whether the input is equal to "Y" or "y"
+	 String a = sc.nextLine(); //decalre variable and type the correct input to answer the question above
+	 if (a.equals(yes)| a.equals(YES)) //determine whether the question "Is the number even?" is true or not
 	 {
 		 System.out.println("Is it divisible by 3?");
-		 String b = sc.nextLine();//
-		 if (b.equals(yes)|b.equals(YES))
+		 String b = sc.nextLine(); //declare variable and type the correct input to answer the question above
+		 if (b.equals(yes)|b.equals(YES)) //determine whether the question "Is it divisible by 3?" is true or not
 		 {
 			 System.out.println("Is your number 6?");
-			 String c = sc.nextLine();
-			 if (c.equals(yes)|c.equals(YES)) System.out.println("Yay!");//
+			 String c = sc.nextLine(); //declare variable and type the correct input to answer the question above
+			 if (c.equals(yes)|c.equals(YES)) //determine whether the question"Is your number 6?" is true or not 
+			 System.out.println("Yay!");//guess the right number and print out "Yay!"
 		 }
 		 else if (b.equals(no)|b.equals(NO)){
 			 System.out.println("Is your number divisible by 4?");
-			 String d = sc.nextLine();
-			 if (d.equals(yes)|d.equals(YES)){
+			 String d = sc.nextLine(); //declare variable and type the correct input to answer the question above
+			 if (d.equals(yes)|d.equals(YES)){ //determine whether the question "Is your number divisible by 4?" is true or not 
 				 System.out.println("Is the number divisible by 4 greater than 1?");
-				 String e = sc.nextLine();
-				 if (e.equals(yes)|e.equals(YES)){
-					 System.out.println("Is your number 8?");
-					 String f = sc.nextLine();
-					 if (f.equals(yes)|f.equals(YES)) System.out.println("Yay!");
+				 String e = sc.nextLine(); //declare variable and type the correct input to answer the question above
+				 if (e.equals(yes)|e.equals(YES)){ //determine whether the question "Is the number divisible by 4 greater than 1?" is true or not
+					 System.out.println("Is your number 8?");//declare variable and type the correct input to answer the question above
+					 String f = sc.nextLine(); //declare variable and type the correct input to answer the question above
+					 if (f.equals(yes)|f.equals(YES)) //determine whether the question "Is your number 8?" is true or not
+					 System.out.println("Yay!"); //guess the right number and print out "Yay!"
 				 }
-				 else if (e.equals(no)|e.equals(NO)){
+				 else if (e.equals(no)|e.equals(NO)){ //the number is not divisible by 4 greater than 1
 					 System.out.println("Is your number 4?");
-					 String g = sc.nextLine();
-					 if (g.equals(yes)|g.equals(YES)) System.out.println("Yay!");
+					 String g = sc.nextLine(); //declare variable and type the correct input to answer the question above
+					 if (g.equals(yes)|g.equals(YES)) //determine whether the question "Is your number 4?" true or not
+					 System.out.println("Yay!"); //guess the right number and print out "Yay!"
 				 }
-				 else System.out.println("Sorry, that is not a valid input.");
+				 else System.out.println("Sorry, that is not a valid input.");//otherwise it is not a valid input
 			 }
-			 else if (d.equals(no)|d.equals(NO)){
+			 else if (d.equals(no)|d.equals(NO)){ //the number is not divisible by 4
 				 System.out.println("Is it divisible by 5?");
-				 String h = sc.nextLine();
-				 if (h.equals(yes)|h.equals(YES)) {
+				 String h = sc.nextLine(); //declare variable and type the correct input to answer the question above
+				 if (h.equals(yes)|h.equals(YES)) { //determine whether the question "Is it divisible by 5?" is true or not
 					 System.out.println("Is your number 10?");
-					 String i = sc.nextLine();
-					 if (i.equals(yes)|i.equals(YES)) System.out.println("Yay!");
+					 String i = sc.nextLine(); //declare variable
+					 if (i.equals(yes)|i.equals(YES)) //determine whether the question "Is your number 10?" is true or not
+					 System.out.println("Yay!"); //guess the right number and print out "Yay!"
 				 }
-				 else if (h.equals(no)|h.equals(NO)){
+				 else if (h.equals(no)|h.equals(NO)){ //the number is not divisible by 5
 					 System.out.println("Is your number 2?");
-					 String j = sc.nextLine();
-					 if (j.equals(yes)|j.equals(YES)) System.out.println("Yay!");
+					 String j = sc.nextLine(); //declare variable
+					 if (j.equals(yes)|j.equals(YES)) //determine whether the question "Is your number 2?" is true or not
+					 System.out.println("Yay!"); //guess the right number and print out "Yay!"
 				 }
-				 else System.out.println("Sorry, that is not a valid input.");
+				 else System.out.println("Sorry, that is not a valid input."); //otherwise it is not a valid input
 			 }
-			 else System.out.println("Sorry, that is not a valid input.");
+			 else System.out.println("Sorry, that is not a valid input."); //otherwise it is not a valid input
 		 }
-		 else System.out.println("Sorry, that is not a valid input.");
+		 else System.out.println("Sorry, that is not a valid input."); //otherwise it is not a valid input
 	 }
 	 
-	 else if (a.equals(no)|a.equals(NO))
+	 else if (a.equals(no)|a.equals(NO)) //the number is not even
 	 {
 		 System.out.println("Is it divisible by 3?");
-		 String k = sc.nextLine();
-		 if(k.equals(yes)|k.equals(YES)){
-			 System.out.println("When devided by 3 is the result greater than 1?");
-			 String l = sc.nextLine();
-			 if (l.equals(yes)|l.equals(YES)){
+		 String k = sc.nextLine();//declare variable
+		 if(k.equals(yes)|k.equals(YES)){ //determine whether the question "Is it divisible by 3?" is true or not
+			 System.out.println("When divided by 3 is the result greater than 1?");
+			 String l = sc.nextLine();//decalre variable
+			 if (l.equals(yes)|l.equals(YES)){ //determine whether the question "When divided by 3 is the result greater than 1?" is true or not
 				 System.out.println("Is your number 9?");
-				 String m = sc.nextLine();
-				 if (m.equals(yes)|m.equals(YES)) System.out.println("Yay!");
+				 String m = sc.nextLine();//decalre variable
+				 if (m.equals(yes)|m.equals(YES)) //determine whether the question "Is your number 9?" is true or not
+				 System.out.println("Yay!"); //guess the right number and print out "Yay!"
 			 }
-			 else if (l.equals(no)|l.equals(NO)){
+			 else if (l.equals(no)|l.equals(NO)){ //when divided by 3 the result is not greater than 1
 				 System.out.println("Is your number 3?");
 				 String n = sc.nextLine();
-				 if (n.equals(yes)|n.equals(YES)) System.out.println("Yay!");
+				 if (n.equals(yes)|n.equals(YES)) System.out.println("Yay!"); //guess the right number and print out "Yay!"
 			 }
-			 else System.out.println("Sorry, that is not a valid input.");
+			 else System.out.println("Sorry, that is not a valid input.");//otherwise it is not a valid input
 		 }
 		 else if (k.equals(no)|k.equals(NO)){
 			 System.out.println("Is it greater than 6?");
@@ -90,7 +95,7 @@ public class WhichNumber {
 			 if (o.equals(yes)|o.equals(YES)){
 				 System.out.println("Is your number 7?");
 				 String p = sc.nextLine();
-				 if (p.equals(yes)|p.equals(YES)) System.out.println("Yay!");
+				 if (p.equals(yes)|p.equals(YES)) System.out.println("Yay!"); //guess the right number and print out "Yay!"
 			 }
 			 else if (o.equals(no)|o.equals(NO)){
 				 System.out.println("Is it less than 3?");
@@ -98,24 +103,25 @@ public class WhichNumber {
 				 if (q.equals(yes)|q.equals(YES)){
 					 System.out.println("Is your number 1?");
 					 String r = sc.nextLine();
-					 if (r.equals(yes)|r.equals(YES)) System.out.println("Yay!");
+					 if (r.equals(yes)|r.equals(YES)) System.out.println("Yay!"); //guess the right number and print out "Yay!"
 				 }
 				 else if (q.equals(no)|q.equals(NO)){
 					 System.out.println("Is your number 5?");
 					 String s = sc.nextLine();
-					 if (s.equals(yes)|s.equals(YES)) System.out.println("Yay!");
+					 if (s.equals(yes)|s.equals(YES)) System.out.println("Yay!"); //otherwise it is not a valid input
 				 }
-				 else System.out.println("Sorry, that is not a valid input.");
+				 else System.out.println("Sorry, that is not a valid input."); //otherwise it is not a valid input
 			 }
-			 else System.out.println("Sorry, that is not a valid input.");
+			 else System.out.println("Sorry, that is not a valid input."); //otherwise it is not a valid input
 		 }
-		 else System.out.println("Sorry, that is not a valid input.");
+		 else System.out.println("Sorry, that is not a valid input."); //otherwise it is not a valid input
 			 
 			 
 	 }
-	 else System.out.println("Sorry, that is not a valid input.");
+	 else System.out.println("Sorry, that is not a valid input."); //otherwise it is not a valid input
 	 
- }
-}
+ } //end of method
+
+} //end of class
 
 		
